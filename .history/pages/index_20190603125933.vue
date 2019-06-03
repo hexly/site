bb5efd3879efd0b2ef743fc866c12480f8df534f
narfdre <narfdre@gmail.com>
2019-06-03 13:04:15 -0600

    more clean up

---
+++ .history/pages/index_20190603125933.vue
@@ -0,0 +1,338 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark -light" id="">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Hexly</a>
+      </div>
+    </nav>
+      <!-- END nav -->
+      
+      <div class="hero-wrap">
+        <div class="overlay"></div>
+        <div class="circle-bg"></div>
+        <div class="circle-bg-2"></div>
+        <div class="container-fluid">
+          <div class="slider-text d-md-flex align-items-center" data-scrollax-parent="true">
+
+            <div class="one-forth pr-md-4 align-self-md-center" data-scrollax=" properties: { translateY: '70%' }">
+              <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Direct Sales, <br> Meet Serverless!</h1>
+              <p class="mb-md-5 mb-sm-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Gone are the days of paying for big fat servers! ;) Scale with your customers.</p>
+              <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="#" class="btn btn-primary px-4 py-3">Request a demo</a></p>
+            </div>
+            <div class="one-half align-self-sm-center">
+              <div class="">
+                <div class="item">
+                  <img src="/images/dashboard_full_1.png" class="img-fluid img" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <section class=" bg-light">
+        <div class="container">
+          <div class="row justify-content-center mb-5 ">
+            <div class="col-md-7 text-center heading-section ">
+              <span class="subheading">Request A Demo</span>
+              <h2 class="mb-4">See what we are all about</h2>
+              <p>We will contact you with more information about what makes Hexly awesome!</p>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-md-8 ">
+              <form action="#" class="domain-form">
+                <div class="form-group d-md-flex">
+                  <input type="text" class="form-control px-4" placeholder="Enter your email address...">
+                  <input type="submit" class="search-domain btn btn-primary px-5" value="Request Demo!">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <section class=" services-section">
+        <div class="container">
+          <div class="row justify-content-center mb-5 pb-5">
+            <div class="col-md-7 text-center heading-section ">
+              <span class="subheading">Services</span>
+              <h2 class="mb-4">Why choose us</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4 d-flex align-self-stretch ">
+              <div class="media block-6 services d-block text-center">
+                <div class="d-flex justify-content-center"><div class="icon"><span class="ion-ios-analytics"></span></div></div>
+                <div class="media-body p-2 mt-3">
+                  <h3 class="heading">Insights</h3>
+                  <p>Get Extra insights into your comp plan.</p>
+                </div>
+              </div>      
+            </div>
+            <div class="col-md-4 d-flex align-self-stretch ">
+              <div class="media block-6 services d-block text-center">
+                <div class="d-flex justify-content-center"><div class="icon"><span class="ion-ios-podium"></span></div></div>
+                <div class="media-body p-2 mt-3">
+                  <h3 class="heading">Technology</h3>
+                  <p>We manage your website traffic dynamically and scale on demand to save you money and meet your distributors needs.</p>
+                </div>
+              </div>    
+            </div>
+            <div class="col-md-4 d-flex align-self-stretch ">
+              <div class="media block-6 services d-block text-center">
+                <div class="d-flex justify-content-center"><div class="icon"><span class="ion-ios-cog"></span></div></div>
+                <div class="media-body p-2 mt-3">
+                  <h3 class="heading">Custom</h3>
+                  <p>Your backoffice will not feel off the shelf</p>
+                </div>
+              </div>      
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="  img" id="section-counter" style="background-image: url(images/bg_1.jpg);">
+        <div class="container">
+          <div class="row justify-content-center mb-5 pb-5">
+            <div class="col-md-7 text-center heading-section heading-section-white ">
+              <h2 class="mb-4">Some fun facts</h2>
+              <span class="subheading">More than 100,000 websites hosted</span>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-md-10">
+              <div class="row">
+                <div class="col-md-3 d-flex justify-content-center counter-wrap ">
+                  <div class="block-18 text-center">
+                    <div class="text">
+                      <strong class="number" data-number="2000">0</strong>
+                      <span>CMS Installation</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3 d-flex justify-content-center counter-wrap ">
+                  <div class="block-18 text-center">
+                    <div class="text">
+                      <strong class="number" data-number="100">0</strong>
+                      <span>Awards Won</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3 d-flex justify-content-center counter-wrap ">
+                  <div class="block-18 text-center">
+                    <div class="text">
+                      <strong class="number" data-number="32000">0</strong>
+                      <span>Registered Domains</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3 d-flex justify-content-center counter-wrap ">
+                  <div class="block-18 text-center">
+                    <div class="text">
+                      <strong class="number" data-number="31998">0</strong>
+                      <span>Satisfied Customers</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class=" testimony-section bg-light">
+        <div class="container">
+          <div class="row justify-content-center mb-5 pb-5">
+            <div class="col-md-7 text-center heading-section ">
+              <span class="subheading">Customer Says</span>
+              <h2 class="mb-4">Our satisfied customer says</h2>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in</p>
+            </div>
+          </div>
+          <div class="row ">
+            <div class="col-md-12">
+              <div class="carousel-testimony">
+                <div class="item">
+                  <div class="testimony-wrap p-4 pb-5">
+                    <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
+                      <span class="quote d-flex align-items-center justify-content-center">
+                        <i class="icon-quote-left"></i>
+                      </span>
+                    </div>
+                    <div class="text">
+                      <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                      <p class="name">Dennis Green</p>
+                      <span class="position">Marketing Manager</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="testimony-wrap p-4 pb-5">
+                    <div class="user-img mb-5" style="background-image: url(images/person_2.jpg)">
+                      <span class="quote d-flex align-items-center justify-content-center">
+                        <i class="icon-quote-left"></i>
+                      </span>
+                    </div>
+                    <div class="text">
+                      <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                      <p class="name">Dennis Green</p>
+                      <span class="position">Interface Designer</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="testimony-wrap p-4 pb-5">
+                    <div class="user-img mb-5" style="background-image: url(images/person_3.jpg)">
+                      <span class="quote d-flex align-items-center justify-content-center">
+                        <i class="icon-quote-left"></i>
+                      </span>
+                    </div>
+                    <div class="text">
+                      <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                      <p class="name">Dennis Green</p>
+                      <span class="position">UI Designer</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="testimony-wrap p-4 pb-5">
+                    <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
+                      <span class="quote d-flex align-items-center justify-content-center">
+                        <i class="icon-quote-left"></i>
+                      </span>
+                    </div>
+                    <div class="text">
+                      <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                      <p class="name">Dennis Green</p>
+                      <span class="position">Web Developer</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="testimony-wrap p-4 pb-5">
+                    <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
+                      <span class="quote d-flex align-items-center justify-content-center">
+                        <i class="icon-quote-left"></i>
+                      </span>
+                    </div>
+                    <div class="text">
+                      <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                      <p class="name">Dennis Green</p>
+                      <span class="position">System Analytics</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="">
+        <div class="container">
+          <div class="row justify-content-center mb-5 pb-5">
+            <div class="col-md-7 text-center heading-section ">
+              <span class="subheading">Services</span>
+              <h2 class="mb-4">How it works</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 nav-link-wrap mb-5 pb-md-5 pb-sm-1 ">
+              <div class="nav  nav-pills justify-content-center text-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" id="v-pills-nextgen-tab" data-toggle="pill" href="#v-pills-nextgen" role="tab" aria-controls="v-pills-nextgen" aria-selected="true">Next gen VPS</a>
+
+                <a class="nav-link" id="v-pills-performance-tab" data-toggle="pill" href="#v-pills-performance" role="tab" aria-controls="v-pills-performance" aria-selected="false">Performance</a>
+
+                <a class="nav-link" id="v-pills-effect-tab" data-toggle="pill" href="#v-pills-effect" role="tab" aria-controls="v-pills-effect" aria-selected="false">Effectiveness</a>
+              </div>
+            </div>
+            <div class="col-md-12 align-items-center ">
+              
+              <div class="tab-content " id="v-pills-tabContent">
+
+                <div class="tab-pane fade show active" id="v-pills-nextgen" role="tabpanel" aria-labelledby="v-pills-nextgen-tab">
+                  <div class="d-md-flex">
+                    <div class="one-forth align-self-center">
+                      <img src="images/dashboard_full_1.jpg" class="img-fluid border" alt="">
+                    </div>
+                    <div class="one-half ml-md-5 align-self-center">
+                      <h2 class="mb-4">Next gen VPS hosting</h2>
+                      <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="tab-pane fade" id="v-pills-performance" role="tabpanel" aria-labelledby="v-pills-performance-tab">
+                  <div class="d-md-flex">
+                    <div class="one-forth order-last align-self-center">
+                      <img src="images/dashboard_full_2.jpg" class="img-fluid border" alt="">
+                    </div>
+                    <div class="one-half order-first mr-md-5 align-self-center">
+                      <h2 class="mb-4">Performance VPS hosting</h2>
+                      <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="tab-pane fade" id="v-pills-effect" role="tabpanel" aria-labelledby="v-pills-effect-tab">
+                  <div class="d-md-flex">
+                    <div class="one-forth align-self-center">
+                      <img src="images/dashboard_full_1.jpg" class="img-fluid border" alt="">
+                    </div>
+                    <div class="one-half ml-md-5 align-self-center">
+                      <h2 class="mb-4">Effective VPS hosting</h2>
+                      <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="-parallax">
+        <div class="parallax-img d-flex align-items-center">
+          <div class="container">
+            <div class="row d-flex justify-content-center">
+              <div class="col-md-7 text-center heading-section heading-section-white ">
+                <h2>Subcribe to our Newsletter</h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in</p>
+                <div class="row d-flex justify-content-center ">
+                  <div class="col-md-6">
+                    <form action="#" class="subscribe-form">
+                      <div class="form-group">
+                        <span class="icon icon-paper-plane"></span>
+                        <input type="text" class="form-control" placeholder="Enter email address">
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer class=" -dark ">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md">
+              <div class="-widget mb-4">
+                <h2 class="-2">Hexly.</h2>
+                <p class="mt-4"><a href="#" class="btn btn-primary p-3">Request a Demo</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+  </div>
+</template>
+
+<style>
+.logo {
+  font-family: 'Pacifico';
+}
+</style>
