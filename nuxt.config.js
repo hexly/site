76510c3454337af58964d9484a5a51eba11b2220
narfdre <narfdre@gmail.com>
2019-06-10 14:09:54 -0600

    fixing the side scrolling thing

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Hexly | Direct Sales Platform | MLM Software',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
       {
         hid: 'description',
         name: 'description',
