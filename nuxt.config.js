16e1e52475c3cc50914ef8e7e80c07d242daa2e3
Jon Sheppard <jsheppard21@gmail.com>
2019-06-10 12:02:25 -0600

    open graph tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,37 @@
 module.exports = {
   head: {
-    title: 'Hexly',
+    title: 'Hexly | Direct Sales Platform | MLM Software',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'title', name: 'title', content: 'MLM Software | Direct Sales Platfom | Hexly' }
-      { hid: 'description', name: 'description', content: 'Hexly\'s platform provides an all-in-one solution for network marketing, direct sales, and party plan companies.' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          "Hexly's platform provides an all-in-one solution for network marketing, direct sales, and party plan companies."
+      },
+      { hid: 'og:title', name: 'og:title', content: 'Hexly' },
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      {
+        hid: 'og:image',
+        name: 'og:image',
+        content:
+          "Hexly's platform provides an all-in-one solution for network marketing, direct sales, and party plan companies."
+      },
+      {
+        hid: 'og:image:secure_url',
+        name: 'og:image:secure_url',
+        content: 'https://www.hexly.io/images/hexly_site.jpg'
+      },
+      { hid: 'og:image:type', name: 'og:image:type', content: 'image/jpeg' },
+      { hid: 'og:image:width', name: 'og:image:width', content: '400' },
+      { hid: 'og:image:height', name: 'og:image:height', content: '300' },
+      {
+        hid: 'og:image:alt',
+        name: 'og:image:alt',
+        content: "Hexly's logo - a capital H in a hexagon"
+      },
+      { hid: 'og:url', name: 'og:url', content: 'https://www.hexly.io' }
     ],
     link: [
       {
