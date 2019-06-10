f81966bf672afdf844b72e7643ff2cde393bfa3e
Jon Sheppard <jsheppard21@gmail.com>
2019-06-10 11:02:11 -0600

    title and meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Hexly' }
+      { hid: 'title', name: 'title', content: 'MLM Software | Direct Sales Platfom | Hexly' }
+      { hid: 'description', name: 'description', content: 'Hexly\'s platform provides an all-in-one solution for network marketing, direct sales, and party plan companies.' }
     ],
     link: [
       {
